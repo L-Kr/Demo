d3cd1c3e5e318685cccc9f8a7cba91c109958b94
Li Rui <987748679@qq.com>
2018-05-24 16:37:12 +0800

    Initial commit

---
+++ Assets/Demo/Demo_Script/Attack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    private float StartTime;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+            other.GetComponent<Animator>().SetBool("Dying", true);
+        if (tag == "Attack1" && (other.tag != "Attack1" && other.tag != "Player"))
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        if (tag == "Attack1")
+            StartCoroutine(Wait());
+        StartTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (tag == "Attack1" && Time.time - StartTime > 5.0f)
+            Destroy(gameObject);
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GetComponent<Rigidbody>().velocity = 20.0f * transform.forward;
+    }
+}
---
+++ Assets/Demo/Demo_Script/Attack2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack2 : MonoBehaviour {
+
+    public ParticleSystem p;
+    public GameObject Exprossion;
+    private float StartTime;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().velocity = transform.forward * 25.0f;
+        StartTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time-StartTime > 3.0f)
+        {
+            Instantiate(Exprossion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+	}
+}
